ec62d0decf4bb3ec3bb5c51b31618fb23a58487b
kfuentesb <kevfueben@gmail.com>
2026-01-21 14:22:27 +0100

    Entidad Game

--- src/main/java/com/game/indie/repository/GameRepository.java
+++ src/main/java/com/game/indie/repository/GameRepository.java
@@ -1,5 +1,10 @@
 package com.game.indie.repository;
 
-public class GameRepository {
+import com.game.indie.entidad.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface GameRepository extends JpaRepository<Game, Integer> {
+    // Aquí no hace falta escribir nada más por ahora.
 }
