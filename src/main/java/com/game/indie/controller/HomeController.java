4d11a555a4ef843a93c3a9e3a95523980495be54
kfuentesb <kevfueben@gmail.com>
2026-02-10 00:10:50 +0100

    Añadido index.html y su controlador

---
+++ src/main/java/com/game/indie/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.game.indie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
